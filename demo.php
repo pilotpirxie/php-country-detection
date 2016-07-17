520380299815e1ef64d9fce598ddf3b35d352e32
Dominik Zdziarski <kontakt@herbaciarz.com>
2016-07-17 15:30:41 +0200

    Better demo

--- demo.php
+++ demo.php
@@ -1,5 +1,9 @@
 <?php
-
+// hook up a library
 require 'CountryDetection.class.php';
-
+// use static method and get country
 echo CountryDetection::get();
+// and it's this same as
+echo CountryDetection::get('country');
+// other nice stuff to detect
+echo CountryDetection::get('currency');
